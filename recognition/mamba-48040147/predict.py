9928f72c1e50712c8ac45ec6c50c10a0030abf7f
kowasaur <75297283+kowasaur@users.noreply.github.com>
2025-10-20 16:00:15 +1000

    change predict.py to use new input/outputs for model

--- recognition/mamba-48040147/predict.py
+++ recognition/mamba-48040147/predict.py
@@ -1,7 +1,8 @@
 import sys
 import cv2
 import torch
-from basicsr.utils import img2tensor, tensor2img, imwrite
+import numpy as np
+from basicsr.utils import img2tensor, imwrite
 from modules import MambaIRv2
 from options import FINAL_MODEL_PATH
 
@@ -11,15 +12,31 @@ if __name__ == "__main__":
         sys.exit(1)
 
     model = MambaIRv2()
-    model.load_state_dict(torch.load(FINAL_MODEL_PATH)["params"], strict=True)
+    # TODO: change to strict=True because should be using trained model
+    model.load_state_dict(torch.load(FINAL_MODEL_PATH)["params"], strict=False)
+    model.eval()
 
     img_path = sys.argv[1]
 
-    img = cv2.imread(img_path) / 255.0
-    img = img2tensor(img, bgr2rgb=True, float32=True)
-    img = img.unsqueeze(0)
+    # Make image grayscale and right format
+    y_img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE) / 255.0
+    y_tensor = np.expand_dims(y_img, axis=2)  # img2tensor needs 3 dimensions
+    y_tensor = img2tensor(y_tensor, float32=True)
+    y_tensor = y_tensor.unsqueeze(0)
+
+    # Inference
     with torch.no_grad():
-        output = model(img)
-    output = tensor2img([output.detach().cpu()])
+        cbcr_tensor = model(y_tensor)
+
+    # Convert tensor to numpy
+    cbcr_np = cbcr_tensor.squeeze(0).cpu().numpy()  # [2,H,W]
+
+    # Stack Y + CbCr: combine grayscale and colour
+    ycbcr = np.stack([y_img, cbcr_np[0], cbcr_np[1]], axis=2)  # [H,W,3]
+
+    # Convert YCbCr -> BGR for OpenCV
+    ycbcr = (ycbcr * 255.0).clip(0, 255).astype(np.uint8)
+    bgr_img = cv2.cvtColor(ycbcr, cv2.COLOR_YCrCb2BGR)
+
     save_path = "output.png"
-    imwrite(output, save_path)
+    imwrite(bgr_img, save_path)
