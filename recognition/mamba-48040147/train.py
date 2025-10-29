37efed8590d26dc5f80778de9293017e6195633e
kowasaur <75297283+kowasaur@users.noreply.github.com>
2025-10-29 18:51:34 +1000

    Remove unnecessary training code
    This is code for resuming state, wandb logging and cuda prefetching that we don't do anyways

--- recognition/mamba-48040147/train.py
+++ recognition/mamba-48040147/train.py
@@ -11,20 +11,17 @@ from os import path as osp
 
 from basicsr.data import build_dataloader, build_dataset
 from basicsr.data.data_sampler import EnlargedSampler
-from basicsr.data.prefetch_dataloader import CPUPrefetcher, CUDAPrefetcher
+from basicsr.data.prefetch_dataloader import CPUPrefetcher
 from basicsr.models import build_model
 from basicsr.utils import (
     AvgTimer,
     MessageLogger,
-    check_resume,
     get_env_info,
     get_root_logger,
     get_time_str,
     init_tb_logger,
-    init_wandb_logger,
     make_exp_dirs,
     mkdir_and_rename,
-    scandir,
 )
 from basicsr.utils.options import dict2str
 from options import (
@@ -82,25 +79,6 @@ def freeze_except_last_and_first(model: modules.MambaIRv2):
         param.requires_grad = True
 
 
-def init_tb_loggers(opt):
-    # initialize wandb logger before tensorboard logger to allow proper sync
-    if (
-        (opt["logger"].get("wandb") is not None)
-        and (opt["logger"]["wandb"].get("project") is not None)
-        and ("debug" not in opt["name"])
-    ):
-        assert (
-            opt["logger"].get("use_tb_logger") is True
-        ), "should turn on tensorboard when using wandb"
-        init_wandb_logger(opt)
-    tb_logger = None
-    if opt["logger"].get("use_tb_logger") and "debug" not in opt["name"]:
-        tb_logger = init_tb_logger(
-            log_dir=osp.join(opt["root_path"], "tb_logger", opt["name"])
-        )
-    return tb_logger
-
-
 def create_train_val_dataloader(opt, logger):
     # create train and val dataloaders
     train_loader, val_loaders = None, []
@@ -156,52 +134,17 @@ def create_train_val_dataloader(opt, logger):
     return train_loader, train_sampler, val_loaders, total_epochs, total_iters
 
 
-def load_resume_state(opt):
-    resume_state_path = None
-    if opt["auto_resume"]:
-        state_path = osp.join("experiments", opt["name"], "training_states")
-        if osp.isdir(state_path):
-            states = list(
-                scandir(state_path, suffix="state", recursive=False, full_path=False)
-            )
-            if len(states) != 0:
-                states = [float(v.split(".state")[0]) for v in states]
-                resume_state_path = osp.join(state_path, f"{max(states):.0f}.state")
-                opt["path"]["resume_state"] = resume_state_path
-    else:
-        if opt["path"].get("resume_state"):
-            resume_state_path = opt["path"]["resume_state"]
-
-    if resume_state_path is None:
-        resume_state = None
-    else:
-        device_id = torch.cuda.current_device()
-        resume_state = torch.load(
-            resume_state_path, map_location=lambda storage, loc: storage.cuda(device_id)
-        )
-        check_resume(opt, resume_state["iter"])
-    return resume_state
-
-
 def train_pipeline(root_path):
     # parse options, set distributed setting, set ramdom seed
     opt = parse_options(root_path)
     opt["root_path"] = root_path
 
+    # Should improve performance
     torch.backends.cudnn.benchmark = True
-    # torch.backends.cudnn.deterministic = True
 
-    # load resume states if necessary
-    resume_state = load_resume_state(opt)
     # mkdir for experiments and logger
-    if resume_state is None and opt["rank"] == 0:
-        make_exp_dirs(opt)
-        if (
-            opt["logger"].get("use_tb_logger")
-            and "debug" not in opt["name"]
-            and opt["rank"] == 0
-        ):
-            mkdir_and_rename(osp.join(opt["root_path"], "tb_logger", opt["name"]))
+    make_exp_dirs(opt)
+    mkdir_and_rename(osp.join(opt["root_path"], "tb_logger", opt["name"]))
 
     # WARNING: should not use get_root_logger in the above codes, including the called functions
     # Otherwise the logger will not be properly initialized
@@ -211,8 +154,12 @@ def train_pipeline(root_path):
     )
     logger.info(get_env_info())
     logger.info(dict2str(opt))
-    # initialize wandb and tb loggers
-    tb_logger = init_tb_loggers(opt)
+
+    # initialize tb logger
+    tb_logger = init_tb_logger(
+        log_dir=osp.join(opt["root_path"], "tb_logger", opt["name"])
+    )
+
     # create train and validation dataloaders
     result = create_train_val_dataloader(opt, logger)
     train_loader, train_sampler, val_loaders, total_epochs, total_iters = result
@@ -226,33 +173,14 @@ def train_pipeline(root_path):
     # freeze all layers except the first and last conv layers
     freeze_except_last_and_first(model.net_g)
 
-    if resume_state:  # resume training
-        model.resume_training(resume_state)  # handle optimizers and schedulers
-        logger.info(
-            f"Resuming training from epoch: {resume_state['epoch']}, iter: {resume_state['iter']}."
-        )
-        start_epoch = resume_state["epoch"]
-        current_iter = resume_state["iter"]
-    else:
-        start_epoch = 0
-        current_iter = 0
+    start_epoch = 0
+    current_iter = 0
 
     # create message logger (formatted outputs)
     msg_logger = MessageLogger(opt, current_iter, tb_logger)
 
     # dataloader prefetcher
-    prefetch_mode = opt["datasets"]["train"].get("prefetch_mode")
-    if prefetch_mode is None or prefetch_mode == "cpu":
-        prefetcher = CPUPrefetcher(train_loader)
-    elif prefetch_mode == "cuda":
-        prefetcher = CUDAPrefetcher(train_loader, opt)
-        logger.info(f"Use {prefetch_mode} prefetch dataloader")
-        if opt["datasets"]["train"].get("pin_memory") is not True:
-            raise ValueError("Please set pin_memory=True for CUDAPrefetcher.")
-    else:
-        raise ValueError(
-            f"Wrong prefetch_mode {prefetch_mode}. Supported ones are: None, 'cuda', 'cpu'."
-        )
+    prefetcher = CPUPrefetcher(train_loader)
 
     # training
     logger.info(f"Start training from epoch: {start_epoch}, iter: {current_iter}")
